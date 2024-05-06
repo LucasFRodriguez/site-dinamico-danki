d472de32cd52efda557bffead1c0f9a7aaed26e6
LucasFRodriguez <lucasfrz26@gmail.com>
2024-05-06 17:44:05 -0300

    finalizando marcação HTML

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 </head>
 <body>
     <header>
+        </div><!--center-->
         <div class="logo">Logomarca</div>
         <nav class="desktop">
             <ul>
@@ -27,17 +28,21 @@
                 <li><a href="">Contato</a></li>
             </ul>
         </nav>
+        </div><!--center-->
     </header>
 
     <section class="banner-principal">
+        <div class="center">
         <form>
             <h2>Qual o seu melhor e-mail?</h2>
             <input type="email" name="email" required />
             <input type="submit" name="acao" value="Cadastrar!" />
         </form>
+        </div><!--center-->
     </section><!--benner-principal-->
 
     <section class="desc-autor">
+        <div class="center">
         <div class="w50" >
         <h2>Lucas Ferreira Rodriguez</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit unde porro illum nostrum iusto deserunt, iste sed quasi maiores cumque vel ratione harum a 
@@ -52,6 +57,81 @@
         <div class="w50">
             <img src="images/eu.png" />
         </div><!--w50-->
+        </div><!--center-->
     </section><!--descrição sobre o autor-->
+
+    <section class="especialidades">
+        <div class="center">
+        <h2 class="title">Especialidades</h2>
+            <div class="box-especialidades">
+                <h3>ICONE 1</h3>
+                <h3>CSS3</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+
+            <div class="box-especialidades">
+                <h3>ICONE 2</h3>
+                <h3>HTML5</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+
+            <div class="box-especialidades">
+                <h3>ICONE 3</h3>
+                <h3>JavaScript</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+        </div><!--center-->
+    </section><!--especialidades-->
+
+    <section class="extras">
+        <div class="center">
+        <div class="w50">
+            <h2 class="title">Depoimentos</h2>
+            <div class="depoimentos-single">
+                <p class="depoimentos-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo fugit laudantium earum, non necessitatibus dignissimos odio nostrum excepturi exercitationem 
+                ulla soluta repudiandae! Quisquam ducimus voluptates doloremque accusamus soluta quaerat, expedita modi qui veniam 
+                quidem ipsa amet eum doloribus dignissimos accusantium.</p>
+                <p class="nome-autor">Lorem Ipsum</p>
+            </div><!--depoimentos-single-->
+
+            <div class="depoimentos-single">
+                <p class="depoimentos-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo fugit laudantium earum, non necessitatibus dignissimos odio nostrum excepturi exercitationem 
+                ulla soluta repudiandae! Quisquam ducimus voluptates doloremque accusamus soluta quaerat, expedita modi qui veniam 
+                quidem ipsa amet eum doloribus dignissimos accusantium.</p>
+                <p class="nome-autor">Lorem Ipsum</p>
+            </div><!--depoimentos-single-->
+
+        </div><!--w50-->
+
+        <div class="w50">
+            <h2 class="title">Serviços</h2>
+            <div class="sevicos">
+            <ul>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+            </ul>
+            </div><!--servicos-->
+        </div><!--w50-->
+        </div><!--center-->
+    </section><!--extras-->
+
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+
 </body>
 </html>
