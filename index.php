87dc6354a940b0aa86bb0937f94cde4cf0c03547
LucasFRodriguez <lucasfrz26@gmail.com>
2024-05-06 16:38:44 -0300

    fazenndo marcação do site utilizando html

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras, chaves, para, o site">
+    <meta name="description" content="SITE DINAMICO FEITO COM O CURSO DANKI CODE DE DESENVOLVEDOR WEB">
+    <title>Site Dinâmico - Dev Rodriguez</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="acao" value="Cadastrar!" />
+        </form>
+    </section><!--benner-principal-->
+
+    <section class="desc-autor">
+        <div class="w50" >
+        <h2>Lucas Ferreira Rodriguez</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit unde porro illum nostrum iusto deserunt, iste sed quasi maiores cumque vel ratione harum a 
+            sequi, eaque veritatis distinctio pariatur nisi voluptate libero! Repellat mollitia ducimus, impedit alias dolor fugit maxime vitae suscipit at ipsa explicabo autem 
+            obcaecati beatae sed sed distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio !
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit unde porro illum nostrum iusto deserunt, iste sed quasi maiores cumque vel ratione harum a 
+            sequi, eaque veritatis distinctio pariatur nisi voluptate libero! Repellat mollitia ducimus, impedit alias dolor fugit maxime vitae suscipit at ipsa explicabo autem 
+            obcaecati beatae sed distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio !
+        </p>
+        </div><!--w50-->
+        <div class="w50">
+            <img src="images/eu.png" />
+        </div><!--w50-->
+    </section><!--descrição sobre o autor-->
+</body>
+</html>
