f7a51ab0e00d89124b343e7088b69a74ef6a9e3c
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-09 12:18:30 -0300

    criando paginas

---
+++ pages/home.php
@@ -0,0 +1,91 @@
+<section class="banner-principal">
+        <div class="overlay"></div><!--overlay-->
+        <div class="center">
+        <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required />
+            <input type="submit" name="acao" value="Cadastrar!" />
+        </form>
+        </div><!--center-->
+    </section><!--benner-principal-->
+    <section class="desc-autor">
+        <div class="center">
+        <div class="w50 left" >
+        <h2>Lucas Ferreira Rodriguez.</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit unde porro illum nostrum iusto deserunt, iste sed quasi maiores cumque vel ratione harum a 
+            sequi, eaque veritatis distinctio pariatur nisi voluptate libero! Repellat mollitia ducimus, impedit alias dolor fugit maxime vitae suscipit at ipsa explicabo autem 
+            obcaecati.
+        </p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus velit unde porro illum nostrum iusto deserunt, iste sed quasi maiores cumque vel ratione harum a 
+            sequi, eaque veritatis distinctio pariatur nisi voluptate libero! Repellat mollitia ducimus, impedit alias dolor fugit maxime vitae suscipit at ipsa explicabo autem 
+            obcaecati beatae sed distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio distinctio !
+        </p>
+        </div><!--w50-->
+        <div class="w50 left">
+            <img class="right" src="<?php echo INCLUDE_PATH; ?>images/eu.png" />
+        </div><!--w50-->
+        <div class="clear"></div>
+        </div><!--center-->
+    </section><!--descrição sobre o autor-->
+    <section class="especialidades">
+        <div class="center">
+        <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
+                <h4>CSS3</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
+                <h4>HTML5</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
+                <h4>JavaScript</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facere ducimus magnam saepe aliquid, 
+                    reprehenderit error voluptate officiis quasi nisi tempora quae, ex, sequi dolorem quaerat odit vel 
+                    quisquam corporis?</p>
+            </div>
+        </div><!--center-->
+        <div class="clear"></div>
+    </section><!--especialidades-->
+    <section class="extras">
+        <div class="center">
+        <div class="w50 left depoimentos-container">
+            <h2 class="title">Depoimentos</h2>
+            <div class="depoimentos-single">
+                <p class="depoimentos-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo fugit laudantium earum, non necessitatibus dignissimos odio nostrum excepturi exercitationem 
+                ulla soluta repudiandae! Quisquam ducimus voluptates doloremque accusamus soluta quaerat, expedita modi qui veniam 
+                quidem ipsa amet eum doloribus dignissimos accusantium.</p>
+                <p class="nome-autor">Lorem Ipsum</p>
+            </div><!--depoimentos-single-->
+            <div class="depoimentos-single">
+                <p class="depoimentos-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo fugit laudantium earum, non necessitatibus dignissimos odio nostrum excepturi exercitationem 
+                ulla soluta repudiandae! Quisquam ducimus voluptates doloremque accusamus soluta quaerat, expedita modi qui veniam 
+                quidem ipsa amet eum doloribus dignissimos accusantium.</p>
+                <p class="nome-autor">Lorem Ipsum</p>
+            </div><!--depoimentos-single-->
+        </div><!--w50-->
+        <div class="w50 left servicos-container">
+            <h2 class="title">Serviços</h2>
+            <div class="sevicos">
+            <ul>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+                <li>orem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit blanditiis repellat enim magnam adipisci, 
+                ab explicabo sed illo</li>
+            </ul>
+            </div><!--servicos-->
+        </div><!--w50-->
+        <div class="clear"></div>
+        </div><!--center-->
+    </section><!--extras-->
