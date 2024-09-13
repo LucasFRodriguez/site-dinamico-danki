c1d8424c45725a1dc0d79afcd7cb50ed10afd207
LucasFRodriguez <lucasfrz26@gmail.com>
2024-09-13 16:49:19 -0300

    paginaContato

--- pages/contato.php
+++ pages/contato.php
@@ -1,1 +1,19 @@
-<h1>Estou na pagina de contato!</h1>
+<div class="contato-container">
+    <div class="center">
+        <form action="">
+            <input type="text" nome="nome" placeholder="Nome...">
+            <div></div>
+            <input type="text" nome="email" placeholder="E-mail...">
+            <div></div>
+            <input type="text" nome="telefone" placeholder="Telefone...">
+            <div></div>
+            <textarea name="mensagem" placeholder="Sua mensagem..."></textarea>
+            <div></div>
+            <input type="submit" name="acao" value="Enviar">
+        </form>
+    </div><!--center-->
+</div><!--contato-container-->
+
+
+<!--Não irei colocar o Map! mas era pra ter! :/-->
+<div id="map">Aqui era pra ter um mapa</div>
